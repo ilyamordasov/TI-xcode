0c0a991f0a17274f2dd71464371770b38631873a
Ilya Mordasov <imordasov@flippingbook.com>
2018-08-07 17:32:33 +0300

    NSTableView added for registers

---
+++ Plot/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Plot
+//
+//  Created by Ilya Mordasov on 06/08/2018.
+//  Copyright © 2018 Ilya Mordasov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
